2d4f8b48f338dd5c44f72e39e0d7a55f62ba06f9
Hemant <hjangid125@gmail.com>
2023-03-27 13:19:34 +0530

    first commit

---
+++ src/main/java/cicd/awsagain/AwsAgainApplication.java
@@ -0,0 +1,13 @@
+package cicd.awsagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsAgainApplication.class, args);
+	}
+
+}
